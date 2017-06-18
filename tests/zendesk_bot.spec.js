23830e603831759908e532425125ca45368c4845
André Mazoni <andre@robos.im>
2017-06-18 16:02:06 -0300

    __formatUpdate now requests the attachments

--- tests/zendesk_bot.spec.js
+++ tests/zendesk_bot.spec.js
@@ -1,3 +1,5 @@
+const nock = require('nock');
+
 const ZendeskBot = require('../lib');
 const config = require('./_config.js');
 
@@ -39,6 +41,11 @@ describe('Zendesk Bot', () => {
 
   it('__formatUpdate correctly formats the update that comes from the API', () => {
     const update = config.sampleUpdate();
+
+    nock('https://mydomain.zendesk.com/api/v2')
+      .get(`/tickets/${update.id}/comments.json`)
+      .reply(200, config.sampleAttachments());
+
     const promise = bot.__formatUpdate(update);
     expect(promise).resolves.toMatchObject({
       raw: config.sampleUpdate(),
@@ -46,14 +53,26 @@ describe('Zendesk Bot', () => {
         id: 20978392,
       },
       recipient: {
+        // TODO: define what should be here
         id: null,
       },
       timestamp: 1304591932000,
       message: {
         mid: 35436,
+        // TODO: define what should be here
         seq: null,
       },
-      attachments: [],
+      attachments: [{
+        type: 'image',
+        payload: {
+          url: 'https://robos.zendesk.com/attachments/token/qj3xHCwPahyITjF5wt912W2ew/?name=image001.gif',
+        },
+      }, {
+        type: 'file',
+        payload: {
+          url: 'https://mydomain.zendesk.com/attachments/token/KjzjWAsbu9tcMiv7ukIaF9yXX/?name=yarn.lock',
+        },
+      }],
     });
   });
 });
